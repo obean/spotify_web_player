e1dfac0eb52bca8da5dc6740f2d1a7e8720094af
ollie beney <ombeney@gmail.com>
2020-12-12 21:19:05 +0000

    spotify connection now exists, verified by playing music from browser using my phone as a controller

--- spotify-player/src/App.js
+++ spotify-player/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import './App.css';
 
 const redirectUri = "http://localhost:3000";
@@ -7,6 +7,8 @@ const scopes = [
   "user-read-playback-state",
 ];
 export const authEndpoint = 'https://accounts.spotify.com/authorize?'
+
+
 
 const hash = window.location.hash
   .substring(1)
@@ -23,15 +25,47 @@ window.location.hash = "";
 
 function App() {
   const [token, setToken] = useState();
+  const [player, setPlayer] = useState();
+
+  let interval = useRef();
 
   useEffect(() => {
       let _token = hash.access_token;
-      console.log(hash.access_token)
       if(_token) {
         setToken(_token)
         console.log(token)
+        checkForPlayer()
       }
-  })
+  }, [])
+
+  useEffect(() => {
+    if(token) {
+      // using an interval as window.Spotify seems slow to mount/exist
+      interval = setInterval(() => checkForPlayer(), 1000);
+    }
+  }, [token])
+  
+  useEffect(() => {
+    if(player){
+    player.connect();
+    console.log("player connect hasn't thrown an error, nice")
+    }
+  }, [player])
+
+  const checkForPlayer = () => {
+    clearInterval(interval)
+    console.log("about to check if window.spotify is null")
+    if(window.Spotify != null) {
+      console.log("window.spotify is defined")
+      setPlayer(new window.Spotify.Player({
+        name: "HomeHub Spotify Player",
+        getOAuthToken: cb => cb(token)
+      }))
+
+   
+      
+    }
+  }
 
 
   return (
@@ -42,6 +76,9 @@ function App() {
               className="btn btn--loginApp-link"
               href={`${authEndpoint}client_id=${process.env.REACT_APP_CLIENTID}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`}
             > Login to Spotify</a>  
+        )}
+        {token && (
+          <h3>{token}</h3>
         )}
   
       </header>
