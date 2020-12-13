b587195c8e66befaf97f19a7db9b4baf4e23c0b4
ollie beney <ombeney@gmail.com>
2020-12-13 12:12:56 +0000

    added a fairly unattractive control bar button set

--- spotify-player/src/components/controlBar.component.js
+++ spotify-player/src/components/controlBar.component.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import './controlBar.css'
 import  pauseButton  from '../controlBarButtons/pauseButton.png'
+import {FaPauseCircle, FaArrowAltCircleLeft, FaArrowAltCircleRight } from 'react-icons/fa';
 function ControlBar(props) {
 
 
@@ -8,10 +9,10 @@ function ControlBar(props) {
   return(
     <div id="controlBar">
       <div class="controlBar-button">
-      <h3>&#10094;</h3>
+      <FaArrowAltCircleLeft/>
       </div>
-      <div class="controlBar-button"><img src={pauseButton} /></div>
-      <div class="controlBar-button"><h3>&#10095;</h3></div>
+      <div class="controlBar-button"> <FaPauseCircle/> </div>
+      <div class="controlBar-button"> <FaArrowAltCircleRight/> </div>
     </div>
   )
 }
