0d8841c9e2e0b4af45d6de2ac84446cbb1032cf8
ollie beney <ombeney@gmail.com>
2020-12-13 17:11:56 +0000

    added #nextTrack

--- spotify-player/src/components/controlBar.component.js
+++ spotify-player/src/components/controlBar.component.js
@@ -12,9 +12,19 @@ function ControlBar(props) {
   }
 
   const resumePlayer = () => {
-    props.player.pause()
+    props.player.resume()
                 .then(() => setIsPlaying(true))
                 .then(() => console.log("playback resumed"))
+  }
+
+  const nextTrack = () => {
+    props.player.nextTrack()
+                .then(() => {
+                  if(!isPlaying) {
+                    setIsPlaying(true)
+                  }
+                })
+                .then(() => console.log("track skipped"))
   }
 
 
@@ -29,7 +39,7 @@ function ControlBar(props) {
       {!isPlaying && (
         <div class="controlBar-button"> <FaPlayCircle onClick={() => resumePlayer()}/> </div>
        )}
-      <div class="controlBar-button"> <FaArrowAltCircleRight/> </div>
+      <div class="controlBar-button"> <FaArrowAltCircleRight onClick={() => nextTrack()}/> </div>
     </div>
   )
 }
