dc16360bcd64181ec63a5d812a26ba3e79f26f5c
ollie beney <ombeney@gmail.com>
2020-12-15 23:52:20 +0000

    closing playlister modal resets currentPlaylist so that you can now click through playlists again

--- spotify-player/src/components/Playlister.component.js
+++ spotify-player/src/components/Playlister.component.js
@@ -45,7 +45,7 @@ const Playlister = ({ playlisterShowing, hide, token, playPlaylist, playPlaylist
               <div className="modal-wrapper" aria-modal aria-hidden tabIndex={-1} role="dialog">
                 <div className="modal">
                   <div className="modal-header">
-                    <button type="button" className="modal-close-button" data-dismiss="modal" aria-label="Close" onClick={hide}>
+                    <button type="button" className="modal-close-button" data-dismiss="modal" aria-label="Close" onClick={() => {hide(); setCurrentPlaylist()}}>
                       <span aria-hidden="true">&times;</span>
                     </button>
                   </div>
